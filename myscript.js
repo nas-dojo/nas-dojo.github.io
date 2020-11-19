956b6541757ccf7b06c2243acdcab58d65a18178
m_dojo <dojo@na-s.jp>
2020-11-19 16:26:34 +0900

    2

--- myscript.js
+++ myscript.js
@@ -1,18 +1,38 @@
 
 var scene = document.querySelector('a-scene');
-var cylinder = document.createElement('a-cylinder');
-cylinder.setAttribute('color', '#FF9500');
-cylinder.setAttribute('height', '2');
-cylinder.setAttribute('radius', '0.75');
-cylinder.setAttribute('position', '3 1 0');
-scene.appendChild(cylinder);
+var sphere = null;
+
+// var sphere = document.createElement('a-sphere');
+// sphere.setAttribute('color', '#FF9500');
+// sphere.setAttribute('radius', '1.25');
+// sphere.setAttribute('position', '0 1.25 -5');
+// scene.appendChild(sphere);
+
+
 
 
 var t = 0;
+var c = 0;
 function render() {
-  t += 0.01;
-  requestAnimationFrame(render);
-  cylinder.setAttribute('position', '3 '+(Math.sin(t*2)+1)+' 0');
+    
+    if (sphere === null) {
+        c = Math.floor(Math.random() * Math.floor(10))-5;
+        sphere = document.createElement('a-sphere');
+        sphere.setAttribute('color', '#FF9500');
+        sphere.setAttribute('radius', '1.25');
+        sphere.setAttribute('position', c + ' 1.25 -5');
+        scene.appendChild(sphere);
+    }
+
+    if (t>100) {
+        scene.removeChild(sphere);
+        sphere = null;
+        t=0;
+    }
+
+    t += 1;
+    requestAnimationFrame(render);
+    sphere.setAttribute('position', c+ ' 0 ' + (-100+t));
 }
 render();
 
