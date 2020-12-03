3646430ebfcde65688058d4a9445c65b0536cbb1
m_dojo <dojo@na-s.jp>
2020-12-03 16:25:40 +0900

    aaa

--- myscript.js
+++ myscript.js
@@ -1,40 +1,40 @@
 
-var scene = document.querySelector('a-scene');
-var sphere = null;
+// var scene = document.querySelector('a-scene');
+// var sphere = null;
 
-// var sphere = document.createElement('a-sphere');
-// sphere.setAttribute('color', '#FF9500');
-// sphere.setAttribute('radius', '1.25');
-// sphere.setAttribute('position', '0 1.25 -5');
-// scene.appendChild(sphere);
+// // var sphere = document.createElement('a-sphere');
+// // sphere.setAttribute('color', '#FF9500');
+// // sphere.setAttribute('radius', '1.25');
+// // sphere.setAttribute('position', '0 1.25 -5');
+// // scene.appendChild(sphere);
 
 
-const ctlL = document.getElementById("ctlL");
-const ctlR = document.getElementById("ctlR");
+// const ctlL = document.getElementById("ctlL");
+// const ctlR = document.getElementById("ctlR");
 
-var t = 0;
-var c = 0;
-function render() {
+// var t = 0;
+// var c = 0;
+// function render() {
     
-    if (sphere === null) {
-        c = Math.floor(Math.random() * Math.floor(10))-5;
-        sphere = document.createElement('a-sphere');
-        sphere.setAttribute('color', '#009500');
-        sphere.setAttribute('radius', '1.25');
-        sphere.setAttribute('position', c + ' 1.25 -5');
-        scene.appendChild(sphere);
-    }
+//     if (sphere === null) {
+//         c = Math.floor(Math.random() * Math.floor(10))-5;
+//         sphere = document.createElement('a-sphere');
+//         sphere.setAttribute('color', '#009500');
+//         sphere.setAttribute('radius', '1.25');
+//         sphere.setAttribute('position', c + ' 1.25 -5');
+//         scene.appendChild(sphere);
+//     }
 
-    t += 1;
-    requestAnimationFrame(render);
-    sphere.setAttribute('position', c+ ' 0 ' + (-100+t));
+//     t += 1;
+//     requestAnimationFrame(render);
+//     sphere.setAttribute('position', c+ ' 0 ' + (-100+t));
     
-    if (t>100) {
-        scene.removeChild(sphere);
-        sphere = null;
-        t=0;
-    }
+//     if (t>100) {
+//         scene.removeChild(sphere);
+//         sphere = null;
+//         t=0;
+//     }
     
-}
-render();
+// }
+// render();
 
